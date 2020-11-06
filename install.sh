494b25ff9355193062550dc1d46f9d62a55f64cd
Liam Godin <lpgodin@wpi.edu>
2020-11-06 10:04:38 -0500

    Add basic install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+# remove old config files and create symlinks from repo
+cd ~
+rm .zshrc .config/nvim/init.vim .config/neofetch/*
+
+ln -s dots/.zshrc .zshrc
+ln -s dots/.config/nvim/init.vim .config/nvim/init.vim
+ln -s dots/.config/neofetch/config.conf .config/neofetch/config.conf
+
+ln -s dots/.config/neofetch/baphomet.txt .config/neofetch/
+ln -s dots/.config/neofetch/ascii.txt .config/neofetch/
