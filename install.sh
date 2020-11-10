fa88d1e1e43e0d699287ebdbead9192821007ea8
Liam Godin <lpgodin@wpi.edu>
2020-11-10 09:21:31 -0500

    Add termite config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # remove old config files and create symlinks from repo
 cd ~
-rm .zshrc .config/nvim/init.vim .config/neofetch/*
+rm .zshrc .config/nvim/init.vim .config/neofetch/* .config/termite/config
 
 ln -s dots/.zshrc .zshrc
 ln -s dots/.config/nvim/init.vim .config/nvim/init.vim
@@ -8,3 +8,5 @@ ln -s dots/.config/neofetch/config.conf .config/neofetch/config.conf
 
 ln -s dots/.config/neofetch/baphomet.txt .config/neofetch/
 ln -s dots/.config/neofetch/ascii.txt .config/neofetch/
+
+ln -s dots/.config/termite/config .config/termite
