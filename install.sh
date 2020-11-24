158c65c2d71f49a14e9b90bff827355ca8f6f612
Liam Godin <lpgodin@wpi.edu>
2020-11-24 11:11:43 -0500

    fix install script and add note update script

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 # remove old config files and create symlinks from repo
-cd ~
-rm .zshrc .config/nvim/init.vim .config/neofetch/* .config/termite/config
+rm ~/.zshrc ~/.config/termite/config
+rm -rf ~/.config/nvim .config/neofetch
 
-ln -s dots/zshrc .zshrc
-ln -s dots/config/nvim/init.vim .config/nvim/init.vim
-ln -s dots/config/neofetch/config.conf .config/neofetch/config.conf
+ln -s ~/dots/zshrc ~/.zshrc
+ln -s ~/dots/config/nvim ~/.config/nvim
+ln -s ~/dots/config/neofetch ~/.config/neofetch
 
-ln -s dots/config/neofetch/baphomet.txt .config/neofetch/
-ln -s dots/config/neofetch/ascii.txt .config/neofetch/
-
-ln -s dots/config/termite/config .config/termite
+ln -s ~/dots/config/termite/config ~/.config/termite/config
